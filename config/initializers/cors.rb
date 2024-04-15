ab0280521a50998df0477ba919e25ccba34110fb
admin1-saurabh <smplrsaurabh30@gmail.com>
2024-04-16 00:48:15 +0530

    Added Frontend link to Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*' # Allow requests from your React frontend origin
+      origins 'https://frontend-home-design.vercel.app' # Allow requests from your React frontend origin
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
