0c906783d4f242d66847c0fd1c971ceb34893dd6
admin1-saurabh <smplrsaurabh30@gmail.com>
2024-04-16 00:04:24 +0530

    Backend

---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*' # Allow requests from your React frontend origin
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
+  
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :cart_items
+  resources :merchants
+  resources :products
+  resources :merchants, only: [:show] do
+    resources :products, only: [:create, :update, :destroy]
+  end
+  resources :cart_items, only: [:create, :index, :destroy]
+  resources :users, only: [:show, :update]
+end
